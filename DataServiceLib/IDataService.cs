8c36cae7a743077e4a904b73a0d505b87fc03fa8
MadsCords <56881674+MadsCords@users.noreply.github.com>
2020-11-12 17:00:28 +0100

    Small fix for StructuredSearchfunction - Not complete though

--- DataServiceLib/IDataService.cs
+++ DataServiceLib/IDataService.cs
@@ -13,7 +13,7 @@ namespace DataServiceLib
         IList<Name_Basics> GetNames();
         //IList<Name_Basics> GetNameId(int id);
         IList<SearchTitleFunction> SearchTitle(int userid, string searchentry);
-        IList<SearchTitleFunction> StructuredSearch(int userid, string entrytitle, string entryplot, string entrycharacters, string entryname);
+        IList<StructuredSearchFunction> StructuredSearch(int userid, string entrytitle, string entryplot, string entrycharacters, string entryname);
         //Product GetProduct(int id);
         Users CreateUser(Users user);
         //void CreateCategory(Category category);
