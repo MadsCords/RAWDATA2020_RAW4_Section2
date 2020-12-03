9977b28d28c85510c3fa78f8811896529979c1df
MadsCords <56881674+MadsCords@users.noreply.github.com>
2020-12-03 12:52:58 +0100

    fixed bootstrap + navbar in index.html

--- WebService/wwwroot/js/main.js
+++ WebService/wwwroot/js/main.js
@@ -1,12 +1,12 @@
-﻿C: \Users\madsc\Source\Repos\RAWDATA_2020_LiveCode5\WebService\wwwroot\css\lib\twitter - bootstrap]\
+﻿
 require.config({
     baseUrl: "js",
     paths: {
         knockout: "lib/knockout/knockout-latest.debug",
         text: "lib/require-text/text.min",
         dataservice: "services/dataService",
-        bootstrap: "css/lib/twitter-bootstrap/js/bootstrap.bundle.min",
-        jqeury: "lib/jquery/jquery.min"
+        bootstrap: "../css/lib/twitter-bootstrap/js/bootstrap.bundle.min",
+        jquery: "lib/jquery/jquery.min"
     },
     shim: {
         bootstrap: ['jquery']
